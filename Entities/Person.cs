5e8f28e633131c81d0c8d9e2b4e3a7f2dab3d1ea
402Martin <402martin@gmail.com>
2024-03-17 17:18:58 -0300

    added main class

--- Entities/Person.cs
+++ Entities/Person.cs
@@ -19,6 +19,10 @@ public class Person
         LastName = lastName;
     }
 
+    public Person()
+    {
+    }
+
     public int? Age {
         get
         {
--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -4,8 +4,19 @@ using Entities;
 string name = "Martin";
 string lastName = "Radovitzky";
 DateTime dateOfBirth = new DateTime(2000, 11, 28);
+
+//diffrent ways to create the Person instance with constructors
 Person person = new Person(name,lastName);
 Person personWithAge = new Person(name,lastName, dateOfBirth);
 
-Console.WriteLine(person.Age);
-Console.WriteLine(personWithAge.Age);
+// with newer declarations:
+Person personDeclaration = new Person
+{
+    Name = name,
+    LastName = lastName,
+    DateOfBirth = dateOfBirth
+};
+
+
+
+
